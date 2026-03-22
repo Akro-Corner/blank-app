bb76f821539e622a9394c919ed0b3f0dce5d40e3
Akro-Corner <lyriumassistance@gmail.com>
2026-03-22 17:43:44 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#-------------------------
+
+st.header("DataSuite - Login")
+st.write("Please enter your credentials and login. Any unauthorized access to the app will be sued.")
+
+if not "logged_user" in st.session_state():
+    lcl_username = st.text_input("User")
+    lcl_password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if lcl_password == st.secrets("ADMIN_PASSWORD"):
+        st.session_state("logged_user") = str(lcl_username)
+        st.switch_page("pages/mainpage.py")
+    else:
+        st.error("Invalid credentials")
