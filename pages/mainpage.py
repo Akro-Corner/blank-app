1bcad2363e29c6a27335656ab2607fab31acab90
Akro-Corner <lyriumassistance@gmail.com>
2026-03-25 18:28:01 +0100

    Add files via upload

--- pages/mainpage.py
+++ pages/mainpage.py
@@ -44,7 +44,7 @@ st.set_page_config(page_title="Registro Pedidos", layout="wide")
 import streamlit as st
 
 # --- 2. Header and Stats ---
-st.logo("images/dark_logo_imprint.png")  
+st.logo("https://streamlit.io/images/brand/streamlit-mark-color.png")
 st.title("DataSuite - Imprint")
 st.divider()
 st.subheader("Bienvenido " + st.session_state["logged_user"])
